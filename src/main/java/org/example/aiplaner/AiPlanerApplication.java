2acccc206e12aa04c106b909ad3a0a00dcd776d1
John Doe <2035160857@qq.com>
2026-06-12 19:29:35 +0800

    0

---
+++ src/main/java/org/example/aiplaner/AiPlanerApplication.java
@@ -0,0 +1,13 @@
+package org.example.aiplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPlanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPlanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/aiplaner/AiPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aiplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPlanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
